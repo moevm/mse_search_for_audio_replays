4f6985fd163f4b9390c8d70cc9b2cd2d6a3932a7
Leonid Parfentiev <leo.parfentiev@gmail.com>
2020-10-01 20:28:27 +0300

    Error out if no files are specified

--- src/main.py
+++ src/main.py
@@ -17,16 +17,13 @@ def main(argv):
         description="""Detect repetitions in audio tracks. In noise
         reduction mode, apply noise reduction to all specified
         files.""")
-    p.add_argument("files", metavar="FILE", type=str, nargs="*",
+    p.add_argument("files", metavar="FILE", type=str, nargs="+",
                    help="""audio files to process (stdin if none
                    specified)""")
     p.add_argument("-d", "--denoise", action="store_true",
                    help="noise reduction mode")
     p.add_argument("-V", "--version", action="version", version="0.0.1")
     args = p.parse_args()
-
-    if not args.files:
-        args.files.append('-')
 
     if args.denoise:
         return denoise(args.files)
